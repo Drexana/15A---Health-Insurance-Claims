c66a818f99d5efbe1a8814f13b3d98abefe8da6a
Sebastian Davalos <sebas06lex@gmail.com>
2025-08-02 20:46:15 -0400

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def load_models():
   iso_models = joblib.load("models/iso_models.joblib")
   return kmeans, scaler, rf_model, iso_models
 
-kmeans, scaler, rf_model, iso_models
+kmeans, scaler, rf_model, iso_models = load_models()
 
 cluster_labels = {0: "Preferred", 1: "High-Cost", 2: "Standard"}
 
@@ -23,7 +23,7 @@ st.markdown("Single Claimant Input")
 
 age = st.number_input("Age", min_value = 18, max_value = 100, value = 35)
 smoker = st.select("Smoker", ["Yes", "No"])
-bmi = st.number_input("BMI", min_value = 10.0, max_value = 60.0, value = 12000.0)
+bmi = st.number_input("BMI", min_value = 10.0, max_value = 60.0, value = 30.0)
 
 smoker_value = 1.0 if smoker == "Yes" else 0.0
 
