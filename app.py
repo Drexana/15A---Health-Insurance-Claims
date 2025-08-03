4076918d243d1c1e3f35f9d4efa97bf38d367932
Sebastian Davalos <sebas06lex@gmail.com>
2025-08-02 21:02:21 -0400

    Update app.py

--- app.py
+++ app.py
@@ -24,6 +24,7 @@ st.markdown("Single Claimant Input")
 age = st.number_input("Age", min_value = 18, max_value = 100, value = 35)
 smoker = st.selectbox("Smoker", ["Yes", "No"])
 bmi = st.number_input("BMI", min_value = 10.0, max_value = 60.0, value = 30.0)
+charges = st.number_input("Annual Charges", min_value=0.0, value=12000.0)
 
 smoker_value = 1.0 if smoker == "Yes" else 0.0
 
